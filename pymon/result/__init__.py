6ef98e75d4f863337d202ab9020cbd5e5cf988df
Ilya Katun <katun.ilya@gmail.com>
2022-05-14 18:56:32 +0300

    Add imports for Result monad

---
+++ pymon/result/__init__.py
@@ -0,0 +1,14 @@
+from .core import (
+    Fail,
+    Ok,
+    Result,
+    Safe,
+    async_safe,
+    if_fail,
+    if_fail_async,
+    if_ok,
+    if_ok_async,
+    safe,
+    safe_unit,
+)
+from .utils import bytes_to_str, bytes_to_str_utf_8, str_to_bytes, str_to_bytes_utf_8
