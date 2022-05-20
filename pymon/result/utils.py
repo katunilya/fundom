2a4b5a40aacec83f54a289df22d013b81f478be1
Ilya Katun <katun.ilya@gmail.com>
2022-05-20 23:55:34 +0300

    Fix docstrings (#18)

--- pymon/result/utils.py
+++ pymon/result/utils.py
@@ -1,20 +1,35 @@
+from pymon.core import hof1
 from pymon.result.core import safe
 
 
-def str_to_bytes(encoding: str):
-    @safe
-    def _str_to_bytes(data: str):
-        return data.encode(encoding)
+@hof1
+@safe
+def str_to_bytes(encoding: str, data: str) -> bytes:
+    """Safely convert `str` to `bytes`.
 
-    return _str_to_bytes
+    Args:
+        encoding (str): to convert with.
+        data (str): to convert.
+
+    Returns:
+        bytes: result.
+    """
+    return data.encode(encoding)
 
 
-def bytes_to_str(encoding: str):
-    @safe
-    def _bytes_to_str(data: bytes):
-        return data.decode(encoding)
+@hof1
+@safe
+def bytes_to_str(encoding: str, data: bytes) -> str:
+    """Safely convert `bytes` to `str`.
 
-    return _bytes_to_str
+    Args:
+        encoding (str): to convert with.
+        data (bytes): to convert.
+
+    Returns:
+        str: result.
+    """
+    return data.decode(encoding)
 
 
 str_to_bytes_utf_8 = str_to_bytes("UTF-8")
