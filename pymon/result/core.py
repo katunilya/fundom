b63c0e34c4c48904eddc0c8cd98bd9aefc4fe9ab
Ilya Katun <katun.ilya@gmail.com>
2022-05-20 23:28:39 +0300

    Add `if_error_returns` utility function (#17)

--- pymon/result/core.py
+++ pymon/result/core.py
@@ -1,6 +1,8 @@
 from functools import wraps
 from typing import Callable, TypeVar
 
+from pymon.core import hof1
+
 V = TypeVar("V")
 T = TypeVar("T")
 TError = TypeVar("TError", bound=Exception)
@@ -32,3 +34,21 @@ def if_error(func: Callable[[TError], V]) -> Callable[[T | TError], T | V]:
                 return t
 
     return _wrapper
+
+
+@hof1
+def if_error_returns(replacement: V, value: T | TError) -> V | T:
+    """Replace `value` with `replacement` if one is `Exception`.
+
+    Args:
+        replacement (V): to replace with.
+        value (T | TError): to replace.
+
+    Returns:
+        V | T: error-safe result.
+    """
+    match value:
+        case Exception():
+            return replacement
+        case _:
+            return value
