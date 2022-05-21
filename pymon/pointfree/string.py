11b56cc6c27bcc8bd74c51a1978ac53596c05dbd
Ilya Katun <katun.ilya@gmail.com>
2022-05-21 11:17:36 +0300

    Add pf `str.count` function (#5)

--- pymon/pointfree/string.py
+++ pymon/pointfree/string.py
@@ -8,3 +8,14 @@ def center(width: SupportsIndex, fillchar: str = " ") -> Callable[[str], str]:
     character (default is a space).
     """
     return lambda data: data.center(width, fillchar)
+
+
+def count(
+    pattern: str, start: SupportsIndex | None = ..., end: SupportsIndex | None = ...
+) -> Callable[[str], int]:
+    """Point-free version of `str.count`.
+
+    Return the number of non-overlapping occurrences of substring sub in string
+    S[start:end]. Optional arguments start and end are interpreted as in slice notation.
+    """
+    return lambda data: data.count(pattern, start, end)
