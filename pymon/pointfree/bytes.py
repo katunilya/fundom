16cdc7059d8fbc160bdae3e7f9426d796d5b15c1
Ilya Katun <katun.ilya@gmail.com>
2022-05-21 23:49:09 +0300

    Add pf `bytes.count` function (#5)

--- pymon/pointfree/bytes.py
+++ pymon/pointfree/bytes.py
@@ -1,6 +1,6 @@
 from typing import SupportsIndex
 
-from pymon.core import hof2
+from pymon.core import hof2, hof3
 
 
 @hof2
@@ -13,3 +13,17 @@ def center(length: SupportsIndex, fillchar: bytes, arg: bytes) -> bytes:
         arg (bytes): to centralize.
     """
     return arg.center(length, fillchar)
+
+
+@hof3
+def count(sub: bytes, arg: bytes) -> int:
+    """Point-free version of `bytes.count`.
+
+    Args:
+        sub (bytes): substring to count.
+        arg (bytes): to count in.
+
+    Returns:
+        int: number of occurances of `pattern` in `arg`.
+    """
+    return arg.count(sub)
