1479ea16abd1a4f3b3bc4e6faafa64f8d2723e7d
Ilya Katun <katun.ilya@gmail.com>
2022-05-22 16:27:14 +0300

    Add predicate `len_less_then` (#9)

--- pymon/predicates.py
+++ pymon/predicates.py
@@ -7,3 +7,9 @@ from pymon.core import hof1
 def len_more_then(length: int, iterable: Iterable) -> bool:
     """If `iterable` length is strictly more than `length`."""
     return length < len(iterable)
+
+
+@hof1
+def len_less_then(length: int, iterable: Iterable) -> bool:
+    """If `iterable` length is strictly less than `length`."""
+    return length > len(iterable)
