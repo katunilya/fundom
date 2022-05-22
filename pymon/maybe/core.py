25326f77388b7204a8f6379f0d1c8cc040349efa
Ilya Katun <katun.ilya@gmail.com>
2022-05-22 22:38:36 +0300

    Rm `maybe/utils` and transfer `some_when` to `maybe/core` (#29)

--- pymon/maybe/core.py
+++ pymon/maybe/core.py
@@ -51,3 +51,21 @@ def if_none_returns(replacement: V, value: T | None) -> V | T:
             return replacement
         case some:
             return some
+
+
+@hof1
+def some_when(predicate: Callable[[T], bool], data: T) -> T | None:
+    """Passes value next only when predicate is True, otherwise returns `None`.
+
+    Args:
+        predicate (Callable[[T], bool]): to fulfill.
+        data (T): to process.
+
+    Returns:
+        T | None: retult.
+    """
+    match predicate(data):
+        case True:
+            return data
+        case False:
+            return None
