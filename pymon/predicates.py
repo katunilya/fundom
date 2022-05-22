c5e7545bfa815abced26f3922d61b1f122823888
Ilya Katun <katun.ilya@gmail.com>
2022-05-22 16:29:30 +0300

    Add predicate `len_less_or_equals` (#9)

--- pymon/predicates.py
+++ pymon/predicates.py
@@ -13,3 +13,9 @@ def len_more_then(length: int, iterable: Iterable) -> bool:
 def len_less_then(length: int, iterable: Iterable) -> bool:
     """If `iterable` length is strictly less than `length`."""
     return length > len(iterable)
+
+
+@hof1
+def len_less_or_equals(length: int, iterable: Iterable) -> bool:
+    """If `iterable` length is less or equals `length`."""
+    return len(iterable) <= length
