57c788158c01ccb304852e40075ea68936650c13
Ilya Katun <katun.ilya@gmail.com>
2022-06-04 22:38:08 +0300

    Fix `Func` usage in `Predicate` (#46)

--- pymon/predicate.py
+++ pymon/predicate.py
@@ -1,63 +1,74 @@
 from dataclasses import dataclass
-from typing import Callable, Generic, Iterable, Sized, TypeVar
+from typing import Callable, Iterable, ParamSpec, Sized, TypeVar
 
-T = TypeVar("T")
+from pymon.core import Func, Future
+
+P = ParamSpec("P")
+TBool = TypeVar("TBool", bool, Future[bool])
 
 
-@dataclass(slots=True)
-class Predicate(Generic[T]):
+@dataclass(slots=True, frozen=True)
+class Predicate(Func[P, TBool]):
     """Abstraction over predicates for seamless composition of predicate functions."""
 
-    _predicate: Callable[[T], bool]
+    func: Callable[P, TBool]
 
-    def __init__(self, predicate: Callable[[T], bool]) -> None:
-        self._predicate = predicate
+    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> TBool:  # noqa
+        return self._predicate(*args, **kwargs)
 
-    def __call__(self, value: T) -> bool:  # noqa
-        return self._predicate(value)
-
-    def _and(self, other: "Predicate[T]") -> "Predicate[T]":
+    def __and__(self, other: "Predicate[P, TBool]") -> "Predicate[P, TBool]":
         return Predicate(lambda v: self._predicate(v) and other._predicate(v))
 
-    def __and__(self, other: "Predicate[T]") -> "Predicate[T]":
-        return self._and(other)
-
-    def _or(self, other: "Predicate[T]") -> "Predicate[T]":
+    def __or__(self, other: "Predicate[P, TBool]") -> "Predicate[P, TBool]":
         return Predicate(lambda v: self._predicate(v) or other._predicate(v))
 
-    def __or__(self, other: "Predicate[T]") -> "Predicate[T]":
-        return self._or(other)
-
-    def _invert(self) -> "Predicate[T]":
+    def __invert__(self) -> "Predicate[P, TBool]":
         return Predicate(lambda v: not self._predicate(v))
 
-    def __invert__(self) -> "Predicate[T]":
-        return self._invert()
 
-
-def predicate(func: Callable[[T], bool]) -> Predicate[T]:
+def predicate(func: Callable[P, TBool]):
     """Makes function composable `Predicate` instance."""
     return Predicate(func)
 
 
-def len_more_then(length: int) -> Predicate[Iterable]:
+def len_more_then(length: int):
     """If `iterable` length is strictly more than `length`."""
-    return Predicate(lambda iterable: length < len(iterable))
+
+    @predicate
+    def _predicate(iterable: Iterable) -> bool:
+        return length < len(iterable)
+
+    return _predicate
 
 
-def len_less_then(length: int) -> Predicate[Iterable]:
+def len_less_then(length: int):
     """If `iterable` length is strictly less than `length`."""
-    return Predicate(lambda iterable: length > len(iterable))
+
+    @predicate
+    def _predicate(iterable: Iterable) -> bool:
+        return length > len(iterable)
+
+    return _predicate
 
 
-def len_less_or_equals(length: int) -> Predicate[Iterable]:
+def len_less_or_equals(length: int):
     """If `iterable` length is less or equals `length`."""
-    return Predicate(lambda iterable: len(iterable) <= length)
+
+    @predicate
+    def _predicate(iterable: Iterable) -> bool:
+        return len(iterable) <= length
+
+    return _predicate
 
 
-def len_more_or_equals(length: int) -> Predicate[Iterable]:
+def len_more_or_equals(length: int):
     """If `iterable` length is more or equals `length`."""
-    return Predicate(lambda iterable: len(iterable) >= length)
+
+    @predicate
+    def _predicate(iterable: Iterable) -> bool:
+        return len(iterable) >= length
+
+    return _predicate
 
 
 TSized = TypeVar("TSized", bound=Sized)
