3f46233e57284b36b7dc1847a6faaa51badcefe3
Ilya Katun <katun.ilya@gmail.com>
2022-06-08 12:16:51 +0300

    Fix bytes `conter` pf func docs (#68)

--- pymon/pointfree/bytes_utils.py
+++ pymon/pointfree/bytes_utils.py
@@ -5,15 +5,15 @@ from pymon.result.core import safe
 
 
 @hof2
-def center(length: SupportsIndex, fillchar: bytes, arg: bytes) -> bytes:
+def center(length: SupportsIndex, fill_char: bytes, arg: bytes) -> bytes:
     """Point-free version of `bytes.center`.
 
     Args:
         length (SupportsIndex): of result bytestring.
-        fillchar (bytes): to fill `bytes` around.
+        fill_char (bytes): to fill `bytes` around.
         arg (bytes): to centralize.
     """
-    return arg.center(length, fillchar)
+    return arg.center(length, fill_char)
 
 
 @hof3
