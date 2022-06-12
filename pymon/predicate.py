6af36078bea37350775e1700d86289ac8b21b43e
Ilya Katun <katun.ilya@gmail.com>
2022-06-12 12:40:19 +0300

    Add `each` and `_each_future` predicate composition classes (#81)

--- pymon/predicate.py
+++ pymon/predicate.py
@@ -1,6 +1,68 @@
 from __future__ import annotations
 
-from typing import Iterable, Sized, TypeVar
+from dataclasses import dataclass
+from typing import Awaitable, Callable, Generic, Iterable, ParamSpec, Sized, TypeVar
+
+from pymon.core import future
+
+P = ParamSpec("P")
+
+
+@dataclass(slots=True, init=False)
+class _each_future(Generic[P]):  # noqa
+    sync_predicates: list[Callable[P, bool]]
+    async_predicates: list[Callable[P, Awaitable[bool]]]
+
+    def __init__(self) -> None:
+        self.sync_predicates = []
+        self.async_predicates = []
+
+    @future.returns
+    async def __call__(self, *args: P.args, **_: P.kwargs) -> bool:
+        if (
+            all(sync_predicate(*args) for sync_predicate in self.sync_predicates)
+            is False
+        ):
+            return False
+
+        for async_predicate in self.async_predicates:
+            if await async_predicate(*args) is False:
+                return False
+
+        return True
+
+    def __lshift__(self, nxt: Callable[P, bool]) -> _each_future[P]:
+        self.sync_predicates.append(nxt)
+        return self
+
+    def __rshift__(self, nxt: Callable[P, Awaitable[bool]]) -> _each_future[P]:
+        self.async_predicates.append(nxt)
+        return self
+
+
+@dataclass(slots=True, init=False)
+class each(Generic[P]):  # noqa
+    """Mathematical conjunction of predicates."""
+
+    predicates: list[Callable[P, bool]]
+
+    def __init__(self) -> None:
+        self.predicates = []
+
+    def __call__(self, *args: P.args, **_: P.kwargs) -> bool:  # noqa
+        return all(predicate(*args) for predicate in self.predicates)
+
+    def __lshift__(self, nxt: Callable[P, bool]) -> each[P]:
+        self.predicates.append(nxt)
+        return self
+
+    def __rshift__(self, nxt: Callable[P, Awaitable[bool]]) -> _each_future[P]:
+        ef = _each_future()
+
+        for predicate in self.predicates:
+            ef = ef << predicate
+
+        return ef >> nxt
 
 
 def len_more_then(length: int):
