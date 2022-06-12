25f6a34314cf0a58f18f223f7ae1d4605dfc8a50
Ilya Katun <katun.ilya@gmail.com>
2022-06-12 13:56:35 +0300

    Add `one` and `_one_future` class for predicate disjunction (#81)

--- pymon/predicate.py
+++ pymon/predicate.py
@@ -65,6 +65,64 @@ class each(Generic[P]):  # noqa
         return ef >> nxt
 
 
+@dataclass(slots=True, init=False)
+class _one_future(Generic[P]):  # noqa
+
+    sync_predicates: list[Callable[P, bool]]
+    async_predicates: list[Callable[P, Awaitable[bool]]]
+
+    def __init__(self) -> None:
+        self.sync_predicates = []
+        self.async_predicates = []
+
+    @future.returns
+    async def __call__(self, *args: P.args, **_: P.kwargs) -> bool:
+        if (
+            any(sync_predicate(*args) for sync_predicate in self.sync_predicates)
+            is True
+        ):
+            return True
+
+        for async_predicate in self.async_predicates:
+            if await async_predicate(*args) is True:
+                return True
+
+        return False
+
+    def __lshift__(self, nxt: Callable[P, bool]) -> _each_future[P]:
+        self.sync_predicates.append(nxt)
+        return self
+
+    def __rshift__(self, nxt: Callable[P, Awaitable[bool]]) -> _each_future[P]:
+        self.async_predicates.append(nxt)
+        return self
+
+
+@dataclass(slots=True, init=False)
+class one(Generic[P]):  # noqa
+    """Mathematical disjunction of predicates."""
+
+    predicates: list[Callable[P, bool]]
+
+    def __init__(self) -> None:
+        self.predicates = []
+
+    def __call__(self, *args: P.args, **_: P.kwargs) -> bool:  # noqa
+        return any(predicate(*args) for predicate in self.predicates)
+
+    def __lshift__(self, nxt: Callable[P, bool]) -> one[P]:
+        self.predicates.append(nxt)
+        return self
+
+    def __rshift__(self, nxt: Callable[P, Awaitable[bool]]) -> _one_future[P]:
+        ef = _one_future()
+
+        for predicate in self.predicates:
+            ef = ef << predicate
+
+        return ef >> nxt
+
+
 def len_more_then(length: int):
     """If `iterable` length is strictly more than `length`."""
 
