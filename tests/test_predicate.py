37a0b40f06fa8556e7534a9b363ee1fa603f68a8
Ilya Katun <katun.ilya@gmail.com>
2022-06-12 13:58:36 +0300

    Add tests for `one` predicate composition class (#81)

--- tests/test_predicate.py
+++ tests/test_predicate.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pymon.predicate import each
+from pymon.predicate import each, one
 
 
 @pytest.mark.parametrize(
@@ -49,3 +49,43 @@ async def test_each_sync_and_async():
     assert await e(6) is True
     assert await e(7) is False
     assert await e(15) is False
+
+
+@pytest.mark.parametrize(
+    "predicates, arg, result",
+    [
+        ([(lambda x: x > 3)], 5, True),
+        ([(lambda x: x > 3), (lambda x: x < 10)], 20, True),
+    ],
+)
+def test_one_sync_only(predicates, arg, result):
+    e = one()
+    for predicate in predicates:
+        e = e << predicate
+
+    assert e(arg) == result
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "predicates, arg, result",
+    [
+        ([more_than_3], 5, True),
+        ([more_than_3, less_than_10], 20, True),
+    ],
+)
+async def test_one_async_only(predicates, arg, result):
+    e = one()
+    for predicate in predicates:
+        e = e >> predicate
+
+    assert await e(arg) == result
+
+
+@pytest.mark.asyncio
+async def test_one_sync_and_async():
+    e = one() << (lambda x: x % 2 == 0) >> (more_than_3) >> (less_than_10)
+
+    assert await e(6) is True
+    assert await e(7) is True
+    assert await e(15) is True
